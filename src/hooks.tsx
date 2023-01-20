bcd2a5a1468c5181745bf3a6de026f270feab9c7
Hector Orellana <hector@zooft.company>
2023-01-19 23:41:52 -0300

    cleanup

--- src/hooks.tsx
+++ src/hooks.tsx
@@ -1,9 +1,6 @@
 import { useState, useEffect } from "react";
 import { isEqual } from "lodash";
 
-import { Command } from "@tauri-apps/api/shell";
-
-import { ExecutableInfo } from "./types";
 import * as commands from "./commands";
 
 export function useWindowDimensions() {
@@ -35,23 +32,6 @@ export function useWindowDimensions() {
   return dimensions;
 }
 
-export function useEntryContent(entryName: string): [string, boolean] {
-  const [content, setContent] = useState("");
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    const fetchEntry = async () => {
-      const newContent = await commands.getEntry(entryName)
-      setContent(newContent);
-      setError(false);
-    };
-
-    fetchEntry().catch((e) => setError(true));
-  }, [entryName])
-
-  return [content, error];
-}
-
 export function usePassEntries(filter: string): any {
   const [entries, setEntries] = useState([]);
 
@@ -68,52 +48,4 @@ export function usePassEntries(filter: string): any {
   }, [filter]);
 
   return [entries, setEntries];
-};
-
-export function usePassPath(): string {
-  const [path, setPath] = useState("");
-
-  const cmd = new Command("get-pass-path");
-
-  cmd.stdout.on("data", (newPath) => {
-    if (newPath !== path) {
-      setPath(newPath);
-    }
-  });
-
-  cmd.on("error", (error) => setPath(error));
-  cmd.spawn().catch(setPath);
-
-  return path;
-};
-
-export function usePassVersion(): string  {
-  const [version, setVersion] = useState("");
-
-  const cmd = new Command("get-pass-version");
-
-  cmd.stdout.on("data", (line) => {
-    if (line.includes("v")) {
-      const newVersion = line
-        .replaceAll("v", "")
-        .replaceAll(" ","")
-        .replaceAll("=", "");
-
-      if (newVersion !== version) {
-        setVersion(newVersion);
-      }
-    }
-  });
-
-  cmd.on("error", (error) => setVersion(error));
-  cmd.spawn().catch(setVersion);
-
-  return version;
-};
-
-export function usePassExecutableInfo(): ExecutableInfo {
-  const path = usePassPath();
-  const version = usePassVersion();
-
-  return { path, version };
 };
--- src/types.tsx
+++ src/types.tsx
@@ -1,8 +1,3 @@
-export interface ExecutableInfo {
-  path: string;
-  version: string;
-};
-
 export interface PassEntry {
   name: string;
   content: string;
